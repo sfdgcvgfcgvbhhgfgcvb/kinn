991bc21b1f34cd1d5cd63bc3f0d11caf066ae416
yamagata kazuma <ubuntu@ip-172-31-40-4.ec2.internal>
2021-10-22 14:37:39 +0000

    kin

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,6 +1,18 @@
 class Tweet < ApplicationRecord
   #TODO: validates
-  belongs_to :user
-  has_many :likes
+  belongs_to :user, dependent: :destroy
+  has_many :likes, dependent: :destroy
   has_many :like_users, through: :likes, source: :user
+  
+  def like(user)
+    likes.create(user_id: user.id)
+  end
+
+  def unlike(user)
+    likes.find_by(user_id: user.id).destroy
+  end
+
+  def liked?(user) 
+    like_users.include?(user)
+  end
 end
